1471e34d88e80c51d5e1cac041715ca87542e23c
Erwin GO <erwingaitano@gmail.com>
2017-06-26 19:02:09 -0500

    adding tests for getChildFiles, getChildFolders

--- _tests/index.js
+++ _tests/index.js
@@ -1,7 +1,51 @@
+require('../url');
 const assert = require('assert');
 const fileHelpers = require('../file');
+const path = require('path');
 
 describe('file helpers', () => {
+  describe('getChildFolders', () => {
+    it('should return correct number of folders', () => {
+      const folders = fileHelpers.getChildFolders(path.join(__dirname, 'examples'));
+      assert.equal(folders.length, 2);
+    });
+
+    it('should return 0 folders if folder does not exist', () => {
+      const folders = fileHelpers.getChildFolders(path.join(__dirname, 'thisDoesNotExist'));
+      assert.equal(folders.length, 0);
+    });
+
+    it('should return correct number of folders recursively', () => {
+      const folders = fileHelpers.getChildFolders(
+        path.join(__dirname, 'examples'),
+        { recursive: true }
+      );
+
+      assert.equal(folders.length, 5);
+    });
+  });
+
+  describe('getChildFiles', () => {
+    it('should return correct number of files', () => {
+      const files = fileHelpers.getChildFiles(path.join(__dirname, 'examples'));
+      assert.equal(files.length, 0);
+    });
+
+    it('should return 0 if folder does not exist', () => {
+      const files = fileHelpers.getChildFiles(path.join(__dirname, 'thisDoesNotExist'));
+      assert.equal(files.length, 0);
+    });
+
+    it('should return correct number of files recursively', () => {
+      const files = fileHelpers.getChildFiles(
+        path.join(__dirname, 'examples'),
+        { recursive: true }
+      );
+
+      assert.equal(files.length, 9);
+    });
+  });
+
   it('getFileBasename: should pass all validations', () => {
     assert.equal(fileHelpers.getFileBasename('erwin.js'), 'erwin');
     assert.equal(fileHelpers.getFileBasename('erwin'), 'erwin');
